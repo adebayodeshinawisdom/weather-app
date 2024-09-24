e9886a506e891d1a40166d417e450f50d2e8a4d8
adebayodeshinawisdom <adebayoadeshinaafeez@gmail.com>
2024-09-24 15:08:03 +0100

    new and latest

---
+++ app/main.js
@@ -0,0 +1,91 @@
+// console.log("Start");
+
+// setTimeout(() => {
+//     console.log("First task done");
+
+//     setTimeout(() => {
+//         console.log("Second task done");
+
+//         setTimeout(() => {
+//             console.log("Third task done");
+
+//             setTimeout(() => {
+//                 console.log("Fourth task done");
+//             }, 1000);
+
+//         }, 1000);
+
+//     }, 1000);
+
+// }, 1000);
+
+// console.log("End");
+
+
+// console.log("Start")
+
+
+// setTimeout(() => {
+//     console.log("First Task done");
+
+//     setTimeout(() => {
+//         console.log("second Task done");
+        
+//         setTimeout(() => {
+//             console.log("Third Task done")
+
+//             setTimeout(() => {
+//                 console.log("Fouth Task done")
+//                 console.log("End")
+//             })
+//         })
+//     })
+// })
+
+
+const myPromise = new Promise((resolve, reject) => {
+    const error = true;
+    if(!error){
+        resolve("Yes! promise has been resolved")
+    }else{
+        reject("No! Promise is not resolved")
+    }
+    
+})
+
+console.log(myPromise)
+
+myPromise.then(value => {
+    return value + " with great progress ";
+})
+.then(newValue => {
+    console.log(newValue)
+})
+.catch(err => {
+    console.error(err)
+})
+
+async function fetchWeather() {
+
+    const apiKey = 'ace9041a797c3d2ca43f6ef25878c51f'; // Your OpenWeatherMap API key
+    const city = 'London';
+    // Corrected URL without single quotes around the API key
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+   
+  }
+
+  fetchWeather()
+
+
+  
