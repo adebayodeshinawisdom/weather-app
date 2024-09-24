1ca045a4bf84393f4c3c6a3af4708550b4b37a16
adebayodeshinawisdom <adebayoadeshinaafeez@gmail.com>
2024-09-24 15:19:54 +0100

    api key

--- app/main.js
+++ app/main.js
@@ -67,10 +67,10 @@ myPromise.then(value => {
 
 async function fetchWeather() {
 
-    const apiKey = 'ace9041a797c3d2ca43f6ef25878c51f'; // Your OpenWeatherMap API key
+     // Your OpenWeatherMap API key
     const city = 'London';
     // Corrected URL without single quotes around the API key
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.apiKey}&units=metric`;
   
     try {
       const response = await fetch(url);
